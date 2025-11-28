89faed77754f0aa48213934189086a7ef2885036
Manuel Mezo Gómez <manumezog@gmail.com>
2025-11-28 06:55:14 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-32625568-d6e6a",
+  "appId": "1:643283622948:web:edeef97e9114ad551cccb1",
+  "apiKey": "AIzaSyADONmjk4C4WfvoZdN2_BxQdXCEevUd1dA",
+  "authDomain": "studio-32625568-d6e6a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "643283622948"
+};
